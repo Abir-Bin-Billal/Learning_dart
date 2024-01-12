0617f71f328063a6399e9038ef280515d6beaeb0
Abir <abir41300@gmail.com>
2024-01-12 23:13:01 +0600

    list and map added

---
+++ map-list.dart
@@ -0,0 +1,17 @@
+void main() {
+  // List abir = ["my name" , "age"];
+  // print(abir);
+var mydetails = {
+    'name': 'Abir',
+    'age' : 29,
+    'born': 2006
+  };
+  print(mydetails);
+
+  mydetails["is_active"] = true;  
+  print(mydetails);
+
+  mydetails['age'] = 30;
+  print(mydetails);
+
+}
