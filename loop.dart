ef9cb6fc88c69b697160e95a5dd970e277e485bb
Abir <abir41300@gmail.com>
2024-01-15 22:49:13 +0600

    switch-case added

---
+++ loop.dart
@@ -0,0 +1,11 @@
+void main() {
+  List myNumber = [
+        01785669162,
+        01764166240,
+        01712728225,
+        018723827373,
+  ];
+  for (int i = 0; i < myNumber.length; i++) {
+    print(myNumber[i]);
+  } 
+}
---
+++ switch-case.dart
@@ -0,0 +1,16 @@
+void main() {
+  String introduction = "Hey";
+
+  switch (introduction) {
+    case 'hello':
+      print('its hello');
+      break;
+    case 'hii':
+      print("its hiii");
+    case "Hey":
+      print("Welcome to real world");
+
+    default:
+      print('Go to new market');
+  }
+}
